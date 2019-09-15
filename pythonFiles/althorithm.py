e7b102b5d8540c441c001268d2863407ada179b0
Stefan Caloian <stefancaloian123@gmail.com>
2019-09-15 14:16:36 -0400

    Update althorithm.py

--- pythonFiles/althorithm.py
+++ pythonFiles/althorithm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from teamStats import teamStats
+import math
 #effective field goal perentage difference: 0.16118265
 #turnover percentage difference: -0.05958713
 #offensive rebound percentage difference: 0.07061777
@@ -18,29 +19,34 @@ from teamStats import teamStats
 #if team A is the host and team B is the visitor,
 #effective field goal percentage is A efgp home - B efgp road
 
-
 teamA_abbr = input("Enter the first team abbreviation: ")
 teamA_year = input("Enter the first team year: ")
+
+teamB_abbr = input("Enter the second team abbreviation: ")
+teamB_year = input("Enter the second team year: ")
 
 avgA_stats = []
 avgB_stats = []
 
 teamA = teamStats(teamA_abbr, teamA_year)
 
-
-teamB_abbr = input("Enter the second team abbreviation: ")
-teamB_year = input("Enter the second team year: ")
 teamB = teamStats(teamB_abbr, teamB_year)
 
-print("print A")
 avgA_stats = teamA.returnValue()
-for x in avgA_stats:
-	print(x)
 
-print("B")
 avgB_stats = teamB.returnValue()
-for y in avgB_stats:
-	print(y)
 
+coefficients = [0.16118265, -0.05958713, 0.07061777, 0.03267933, 0.17885523, -0.33924331]
 
-    
+x = 0;
+i = 0;
+
+while i <= 5:
+	x+= (avgA_stats[i]-avgB_stats[i]) * coefficients[i]
+	i+=1
+
+sigmoid = 1 / (1 + math.exp(-x));
+if sigmoid > 0.5:
+	print(teamA_abbr + ' Wins!')
+else:
+	print(teamB_abbr + ' Wins!')
